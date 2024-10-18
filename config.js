2cbcf7fe594e29a9e3bf64ae0fc63783f6613b60
evanZccc <167677714+evanZccc@users.noreply.github.com>
2024-10-18 21:01:42 +0200

    config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "lavalink.clxud.dev",
+        port: 2333,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
